b2e2092659b9ed26a4bf0648c2884aa0a86044b9
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-09-07 17:32:17 -0300

    Remoção do limit dos resources

--- dbpedia/enriquecimento_subject.php
+++ dbpedia/enriquecimento_subject.php
@@ -181,7 +181,7 @@ $resources = array(
 $resources = array(array("resource" => "http://dbpedia.org/resource/Influenza"));
 
 
-$tweets = query("SELECT * FROM conceito WHERE sucesso = 1 AND resourceCompleto IS NOT NULL LIMIT 2");
+$tweets = query("SELECT * FROM conceito WHERE sucesso = 1 AND resourceCompleto IS NOT NULL");
 foreach (getRows($tweets) as $key => $conceito) {
 
 //foreach ($resources as $key => $conceito) {
