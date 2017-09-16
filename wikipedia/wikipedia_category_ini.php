d7b657910ab37d35f02bf0a91681ba6e8b8bf4de
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-09-16 14:32:09 -0300

    Chupa cabra Cat -> catLink

--- wikipedia/wikipedia_category_ini.php
+++ wikipedia/wikipedia_category_ini.php
@@ -66,7 +66,7 @@ function getArvoreCompletaNew($category, $nivel = 0) {
 }
 
 function getCategoriesTNew($category) {
-	$cats = Blockspring::runParsed("get-wikipedia-sub-categories", array("category" => "Ice hockey leagues in Canada"), array("api_key" => "br_71919_172db6c45274f6e73c502ef6698223c78d875639"))->params;
+	$cats = Blockspring::runParsed("get-wikipedia-sub-categories", array("category" => $category), array("api_key" => "br_71919_172db6c45274f6e73c502ef6698223c78d875639"))->params;
 	return $cats["subcategories"];
 }
 
@@ -174,7 +174,7 @@ foreach (getRows($tweets) as $key => $value) {
 	$tree = getCategoriesTNew($value["category"]);
 	foreach ($tree as $keyCat => $valueCat) {
 		try {
-			$insert = "INSERT INTO `category_link` (categoriaPai, categoriaFilho) VALUES ('" . escape($value["category"]) . "', '" . escape($valueCat) . "');";
+			$insert = "INSERT INTO `category_link` (categoriaPai, categoriaFilho) VALUES ('" . escape($value["category"]) . "', '" . escape("Category:" . $valueCat) . "');";
 			query($insert, false);
 		} catch (Exception $e) {}
 	}
