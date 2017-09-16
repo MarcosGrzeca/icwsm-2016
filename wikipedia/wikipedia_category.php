aba2d6d3ea4a5e898d61d808d96dfd13915c22b0
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-09-16 13:53:07 -0300

    Ajuste nao pegar mesmas categorias

--- wikipedia/wikipedia_category.php
+++ wikipedia/wikipedia_category.php
@@ -176,7 +176,7 @@ $categoryTree = array();
 
 $ind = 0;
 while (true) {
-	$tweets = query("SELECT * FROM conceito WHERE sucesso = 1 AND wikiID IS NOT NULL LIMIT 10");
+	$tweets = query("SELECT * FROM conceito c WHERE sucesso = 1 AND wikiID IS NOT NULL AND NOT EXISTS (SELECT w.resource FROM wikipedia_category w WHERE w.resource = c.resource) LIMIT 10");
 	if (getNumRows($tweets) == 0) {
 		echo 'FIM';
 		break;
