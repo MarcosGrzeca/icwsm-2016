d0c645e071d5f00ecd592eb7e57f2b58587e32db
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-09-16 15:29:08 -0300

    DIIEEEEEEEEeee

--- wikipedia/wikipedia_category.php
+++ wikipedia/wikipedia_category.php
@@ -220,9 +220,9 @@ while (true) {
 				try {
 					$insert = "INSERT INTO `wikipedia_category` (resource, category) VALUES ('" . escape(array_search($key, $wikis)) . "', '" . escape($categoria["nome"]) . "');";
 					if (array_search($key, $wikis) == "") {
-						/*var_export($key);
+						var_export($key);
 						var_export($wikis);
-						die("Resource em branco " . $key);*/
+						die("Resource em branco " . $key);
 						break 2;
 					}
 					query($insert, false);
