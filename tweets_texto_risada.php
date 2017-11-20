f9140a11b7d2bed5c7826cffc9426aefde38f529
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-11-20 21:41:31 -0200

    RISADA

--- tweets_texto_risada.php
+++ tweets_texto_risada.php
@@ -121,7 +121,7 @@ foreach (getRows($tweets) as $key => $value) {
 		$update = "UPDATE `tweets` SET textoParserRisadaEmoticom = '" . mysqli_real_escape_string(Connection::get(), $textoFull) . "' WHERE id = "  . $value["id"];
         //debug($textoFull);
         //debug($update);
-        //query($update);
+        query($update);
 	} catch (Exception $e) {
 		debug("ERRO");
 		debug($e->getMessage());		
